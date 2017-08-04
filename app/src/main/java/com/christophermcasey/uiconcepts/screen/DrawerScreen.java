d54a4106ca8a74c7e3137ccc45b1036a4c9f6adf
Christopher Casey <christopher.casey@TCs-MacBook-Pro.local>
2017-08-04 13:41:38 -0500

    update dagger

--- app/src/main/java/com/christophermcasey/uiconcepts/screen/DrawerScreen.java
+++ app/src/main/java/com/christophermcasey/uiconcepts/screen/DrawerScreen.java
@@ -2,20 +2,20 @@ package com.christophermcasey.uiconcepts.screen;
 
 import android.content.res.Resources;
 import android.support.annotation.Nullable;
-import dagger.Subcomponent;
-import javax.inject.Singleton;
-import com.christophermcasey.uiconcepts.R;
-import com.christophermcasey.uiconcepts.AppComponent;
 import com.christophermcasey.appcore.dagger.DaggerService;
 import com.christophermcasey.appcore.mortarflow.Layout;
 import com.christophermcasey.appcore.mortarflow.Screen;
+import com.christophermcasey.appcore.mortarflow.scope.PerScreen;
+import com.christophermcasey.uiconcepts.AppComponent;
+import com.christophermcasey.uiconcepts.R;
 import com.christophermcasey.uiconcepts.view.DrawerView;
+import dagger.Subcomponent;
 import mortar.MortarScope;
 
 @Layout(R.layout.drawer_view)
 public class DrawerScreen extends Screen {
 
-  @Singleton
+  @PerScreen
   @Subcomponent
   public interface DrawerScreenComponent {
     void inject(DrawerView DrawerView);
